55f93ad5c9501614350b3a760cd06e9c0e06f3a7
amine <amine.hadef@epitech.eu>
2019-03-22 21:32:09 +0100

    mise en place du system d'evenement et du system direction automatique

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ class snack{
     constructor(x, y){
         this.x = x
         this.y = y
+        this.interva
     }
 
     drawBlock(){
@@ -13,16 +14,16 @@ class snack{
         ctx.fillRect(this.x,this.y, 10, 5)
     }
     moveRigth(){
-        this.x = this.x + 10
+        this.x = this.x + 5
     }
     moveLeft(){
-        this.x = this.x - 10
+        this.x = this.x - 5
     }
     moveUp(){
-        this.y = this.y + 5
+        this.y = this.y + 2
     }
     moveBottom(){
-        this.y = this.y - 5
+        this.y = this.y - 2
     }
     deleteBlock(){
         ctx.clearRect(this.x,this.y, 10, 5);
@@ -31,49 +32,52 @@ class snack{
 
 let snack1 = new snack(10,10)
 
-window.addEventListener('keyup', (e)=>{
+window.addEventListener('keydown', (e)=>{
     e.preventDefault()
-
+    window.clearInterval(snack1.interva)
     switch (e.keyCode) {
-
-        case 39:// move rigth
-            if (dirction == 'left') {
-                break
-            }
+        
+        case 39:
+            if (dirction == 'left') {break}
             dirction = 'rigth'
-            snack1.deleteBlock()
-            snack1.moveRigth()
-            snack1.drawBlock()
+            snack1.interva = window.setInterval(()=>{
+                snack1.deleteBlock()
+                snack1.moveRigth()
+                snack1.drawBlock()
+            },250)
+
         break
 
-        case 37://move left
-            if (dirction == 'rigth') {
-                break
-            }
-            dirction = 'left'
-            snack1.deleteBlock()
-            snack1.moveLeft()
-            snack1.drawBlock()
-        break
-
-        case 38:// move bottom
-            if (dirction == 'up') {
-                break
-            }
+        case 38:
+            if (dirction == 'up') {break}
             dirction = 'bottom'
-            snack1.deleteBlock()
-            snack1.moveBottom()
-            snack1.drawBlock()
+            snack1.interva = window.setInterval(()=>{
+                snack1.deleteBlock()
+                snack1.moveBottom()
+                snack1.drawBlock()
+            },250)
+
         break
 
-        case 40:// move up
-            if (dirction == 'bottom') {
-                break
-            }
+        case 37:
+            if (dirction == 'rigth') {break}
+            dirction = 'left'
+            snack1.interva = window.setInterval(()=>{
+                snack1.deleteBlock()
+                snack1.moveLeft()
+                snack1.drawBlock()
+            },250)
+            
+        break
+
+        case 40:
+            if (dirction == 'bottom') {break}
             dirction = 'up'
-            snack1.deleteBlock()
-            snack1.moveUp()
-            snack1.drawBlock()
+            snack1.interva = window.setInterval(()=>{
+                snack1.deleteBlock()
+                snack1.moveUp()
+                snack1.drawBlock()
+            },250)
         break
     }
 })
