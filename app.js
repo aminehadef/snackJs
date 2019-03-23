9222c62111d6d496f0ce7ff35ae2d4363f43e25d
amine <amine.hadef@epitech.eu>
2019-03-23 05:11:53 +0100

    mise en place du system de collision serpent pomme

--- app.js
+++ app.js
@@ -3,17 +3,20 @@ let ctx = canvas.getContext('2d')
 let dirction  = ''
 let canvasWidth = canvas.width
 let canvasHeight = canvas.height
-
+let scor = 0
+let vitesse = 25
 class Snack{
     constructor(x, y){
         this.x = x
         this.y = y
+        this.width = 10
+        this.height = 5
         this.interva
     }
 
     drawBlock(){
         ctx.fillStyle = 'white'
-        ctx.fillRect(this.x,this.y, 10, 5)
+        ctx.fillRect(this.x,this.y, this.width, this.height)
     }
     deleteBlock(){
         ctx.clearRect(this.x,this.y, 10, 5)
@@ -39,7 +42,6 @@ class Appel{
         this.y = y
     }
     drawAppel(){
-        ctx.fillStyle = 'red'
         ctx.beginPath()
         ctx.fillStyle="#FF4422"
         ctx.arc(this.x,this.y + 2, 2, 0, 10 * Math.PI)
@@ -47,12 +49,22 @@ class Appel{
     }
 }
 
-let snack1 = new Snack(10,10)
+let snack1 = new Snack(50,15)
+let appel1 = new Appel(45,65)
+
+function collision(){
+    if (snack1.x < appel1.x + 2 &&
+        snack1.x + snack1.width > appel1.x - 2 &&
+        snack1.y < appel1.y + 4 &&
+        snack1.height + snack1.y > appel1.y)
+        {
+        scor++
+        console.log('miamiam');
+    }
+}
 
 
-let appel1 = new Appel(50,10)
 
-appel1.drawAppel()
 
 window.addEventListener('keydown', (e)=>{
     e.preventDefault()
@@ -70,7 +82,8 @@ window.addEventListener('keydown', (e)=>{
                 snack1.deleteBlock()
                 snack1.moveRigth()
                 snack1.drawBlock()
-            },25)
+                collision()
+            },vitesse)
 
         break
 
@@ -79,16 +92,15 @@ window.addEventListener('keydown', (e)=>{
             if (dirction == 'bottom') {break}
             window.clearInterval(snack1.interva)
             dirction = 'up'
-
             snack1.interva = window.setInterval(()=>{
-                
                 if (snack1.y < 2) {
                     window.clearInterval(snack1.interva)
                 }
                 snack1.deleteBlock()
                 snack1.moveUp()
                 snack1.drawBlock()
-            },25)
+                collision()
+            },vitesse)
 
         break
 
@@ -103,7 +115,8 @@ window.addEventListener('keydown', (e)=>{
                 snack1.deleteBlock()
                 snack1.moveLeft()
                 snack1.drawBlock()
-            },25)
+                collision()
+            },vitesse)
             
         break
 
@@ -118,8 +131,10 @@ window.addEventListener('keydown', (e)=>{
                 snack1.deleteBlock()
                 snack1.moveBottom()
                 snack1.drawBlock()
-            },25)
+                collision()
+            },vitesse)
         break
     }
 })
 snack1.drawBlock()
+appel1.drawAppel()
