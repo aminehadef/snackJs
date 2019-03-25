8d332b56d223810fc2444a23211a4a5c7eb056eb
amine <amine.hadef@epitech.eu>
2019-03-25 21:53:50 +0100

    co

--- js/index.js
+++ js/index.js
@@ -1,3 +1,6 @@
+/**
+ * class snack représente la tête du serpent
+ */
 class Snack{
     constructor(x, y){
         this.x = x
@@ -28,6 +31,9 @@ class Snack{
         this.y = this.y + 1
     }
 }
+/**
+ * class Appel représente la pomme que doit manger le serpent
+ */
 class Appel{
     constructor(x,y){
         this.x = x
@@ -40,6 +46,9 @@ class Appel{
         ctx.fill()
     }
 }
+/**
+ * class scutSnack représente la queue du serpent
+ */
 class ScutSnack{
     constructor (x, y, snackX,snackY){
         this.x = x
@@ -71,43 +80,58 @@ class ScutSnack{
     }
 }
 
+/**
+ * init canvas
+ */
 let canvas = document.getElementById('canvas')
 let ctx = canvas.getContext('2d')
-let dirction  = ''
 let canvasWidth = canvas.width
 let canvasHeight = canvas.height
+
+/**
+ * variable du jeux
+ */
+
 let scor = 0
 let vitesse = 25
+let dirction  = ''
 
+/**
+ * init pour un premier affichage
+ */
 let snack1 = new Snack(50,15)
 let appel1 = new Appel(45,65)
 let scut = new ScutSnack(38,15, snack1.x, snack1.y)
 
 scut.drawBlock()
 
+/**
+ * fonction pour la détection de collision
+ */
 function collision(){
 
     if (snack1.x < appel1.x + 2 && snack1.x + snack1.width > appel1.x - 2 && snack1.y < appel1.y + 4 && snack1.height + snack1.y > appel1.y){
         scor++
         if (dirction == 'bottom') {
-            //snackSize.push( new Snack(snack1.x, snack1.y - (snack1.height + 5)))
-            //console.log(snackSize)
+            console.log(snack1.x);
         }
         if (dirction == 'up') {
-            //snackSize.push( new Snack(snack1.x, snack1.y + (snack1.height + 5)))
-            //console.log(snackSize)
+            console.log(snack1.x);
         }
         if (dirction == 'rigth') {
-            //snackSize.push( new Snack(snack1.x - snack1.width - 5, snack1.y))
-            //console.log(snackSize)
+            console.log(snack1.x);
         }
         if (dirction == 'left') {
-            //snackSize.push( new Snack(snack1.x + snack1.width + 5, snack1.y))
-            //console.log(snackSize)
+            console.log(snack1.x);
         }
     }
 }
 
+/**
+ * init d'evenement des touche directionnel
+ * direction reprensent la direction acctuel qui enpeche des comportement illogique du serpent
+ * cette variable change a chaque fois qu'une touche directionnel est appuyer attention cette variable est null au debut du proramme
+ */
 
 window.addEventListener('keydown', (e)=>{
     e.preventDefault()
@@ -115,13 +139,14 @@ window.addEventListener('keydown', (e)=>{
     switch (e.keyCode) {
         
         case 39:
-            if (dirction == 'left') {break}
-            window.clearInterval(snack1.interva)
+            if (dirction == 'left') {break}// vérifie la direction du serpent pour éviter les demitour
+            window.clearInterval(snack1.interva)// pour éviter les multiplier les setinterval
             dirction = 'rigth'
             snack1.interva = window.setInterval(()=>{
-                if (snack1.x + 14 > canvasWidth) {
+                if (snack1.x + 14 > canvasWidth) {//la tête du serpent est au bord de la canvas 
                     window.clearInterval(snack1.interva)
                 }
+                collision()
                 snack1.deleteBlock()
                 snack1.moveRigth()
                 snack1.drawBlock()
@@ -151,6 +176,7 @@ window.addEventListener('keydown', (e)=>{
                 if (snack1.y < 2) {
                     window.clearInterval(snack1.interva)
                 }
+                collision()
                 snack1.deleteBlock()
                 snack1.moveUp()
                 snack1.drawBlock()
@@ -181,6 +207,7 @@ window.addEventListener('keydown', (e)=>{
                 if (snack1.x < 4) {
                     window.clearInterval(snack1.interva)
                 }
+                collision()
                 snack1.deleteBlock()
                 snack1.moveLeft()
                 snack1.drawBlock()
@@ -209,6 +236,7 @@ window.addEventListener('keydown', (e)=>{
                 if (snack1.y + 7 > canvasHeight) {
                     window.clearInterval(snack1.interva)
                 }
+                collision()
                 snack1.deleteBlock()
                 snack1.moveBottom()
                 snack1.drawBlock()
